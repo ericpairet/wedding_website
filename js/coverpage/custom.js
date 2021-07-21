e7b69d30fc215d71365367cb446b2e730b31386a
Èric Pairet <ericpairet@gmail.com>
2021-07-21 15:13:00 +0100

    styling coverpage

--- js/coverpage/custom.js
+++ js/coverpage/custom.js
@@ -33,31 +33,19 @@ $(document).ready(function() {
   $(function() {
     $('body').vegas({
         slides: [
-            // { src: 'images/slide-img1.jpg' },
-            // { src: 'images/slide-img2.jpg' },
-            // { src: 'images/slide-img3.jpg' }
 						{ src: 'img/coverpage/20210423_135107.jpg' },
 						{ src: 'img/coverpage/20210402_141336.jpg' },
 						{ src: 'img/coverpage/20170811_103120.jpg' },
-						{ src: 'img/coverpage/20170813_100909.jpg' },
             { src: 'img/coverpage/20171216_113958.jpg' },
-            { src: 'img/coverpage/20180102_104358.jpg' },
             { src: 'img/coverpage/20180615_120626.jpg' },
-            { src: 'img/coverpage/20180904_120053.jpg' },
-            { src: 'img/coverpage/20181022_144203.jpg' },
-            { src: 'img/coverpage/20181022_182519.jpg' },
-            { src: 'img/coverpage/20190326_093800.JPG' },
             { src: 'img/coverpage/20190327_092044.jpg' },
-            { src: 'img/coverpage/20190327_092550.jpg' },
-            { src: 'img/coverpage/20191031_220900.jpg' },
             { src: 'img/coverpage/20191101_111109.jpg' },
             { src: 'img/coverpage/20191226_141303.jpg' },
             { src: 'img/coverpage/20200903_161959.jpg' },
-            { src: 'img/coverpage/20201209_160530.jpg' },
-            { src: 'img/coverpage/20210213_134924.jpg' }
+						{ src: 'img/coverpage/20190327_092550.jpg' } // DO NOT PUT NEXT TO 20190327_092044
 				// ]
         ],
-        timer: false// ,
+        timer: false
         // transition: ['zoomIn'], // transitions=["fade","fade2","blur","blur2","flash","flash2","negative","negative2","burn","burn2","slideLeft","slideLeft2","slideRight","slideRight2","slideUp","slideUp2","slideDown","slideDown2","zoomIn","zoomIn2","zoomOut","zoomOut2","swirlLeft","swirlLeft2","swirlRight","swirlRight2"]
         // animation: ['kenburns'] // animations=["kenburns","kenburnsLeft","kenburnsRight","kenburnsUp","kenburnsUpLeft","kenburnsUpRight","kenburnsDown","kenburnsDownLeft","kenburnsDownRight"]
     });
